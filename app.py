8cbd10d2b00d03123f51090a7b2afaa09f935d38
Daniel <60417296+DaHyper@users.noreply.github.com>
2025-10-28 14:04:14 -0400

    fix app py

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ google = oauth.register(
     name='google',
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://oauth2.googleapis.com/token',
-    authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    client_kwargs={'scope': 'openid email profile'},
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration'
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
 )
 
 # --- Routes ---
